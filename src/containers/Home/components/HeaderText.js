301b276c1cc11bbc75cde97be116f7f50f93b882
tienthanght96 <trantienthanght96@gmail.com>
2018-05-21 20:53:39 +0700

    Updates

--- src/containers/Home/components/HeaderText.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import '../Home.scss';
-const HeaderText = ({title}) => (
-  <div className="container-header-text">
-    <h3 className="title-text">{title}</h3>
-  </div>
-);
-export default HeaderText;
